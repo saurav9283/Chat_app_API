2522047f30ceac37b1c61a2b72ecc6a50ce7a4f3
Saurav <sauravkumar00108@gmail.com>
2023-08-06 17:15:06 +0530

    last connit

---
+++ controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require("../model/userModel.js");
+const brcypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+      const usernameCheck = await User.findOne({ username });
+      // console.log( username)
+      if (usernameCheck)
+        // return  res.status(203).send("Username already used");
+        return res.json({ msg: "Username already used", status: false });
+      const emailCheck = await User.findOne({ email });
+      if (emailCheck)
+        // return res.status(203).send("Email already used");
+        return res.json({ msg: "Email already used", status: false });
+        const hashedPassword = await brcypt.hash(password, 10);
+      const user = await User.create({
+        email,
+        username,
+        password: hashedPassword,
+      });
+      delete user.password;
+      // return res.status(200).send(req.body)
+      return res.json({ status: true, user });
+    } catch (ex) {
+      next(ex);
+    }
+  };
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const userRoutes = require("../server/routes/userRoutes.js")
 
 const app = express();
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth" , userRoutes) // static url
 
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
