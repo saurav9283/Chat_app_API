326b854cadb787de594da31f8927d64848975e2e
Saurav <sauravkumar00108@gmail.com>
2023-08-06 17:34:24 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,12 +7,12 @@ module.exports.register = async (req, res, next) => {
       const usernameCheck = await User.findOne({ username });
       // console.log( username)
       if (usernameCheck)
-        // return  res.status(203).send("Username already used");
-        return res.json({ msg: "Username already used", status: false });
+        return  res.status(203).send("Username already used");
+        // return res.json({ msg: "Username already used", status: false });
       const emailCheck = await User.findOne({ email });
       if (emailCheck)
-        // return res.status(203).send("Email already used");
-        return res.json({ msg: "Email already used", status: false });
+        return res.status(203).send("Email already used");
+        // return res.json({ msg: "Email already used", status: false });
         const hashedPassword = await brcypt.hash(password, 10);
       const user = await User.create({
         email,
